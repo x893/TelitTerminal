4622b9f42aa31912fab8efc2271ef12b4b74b99b
x893 <x893.00@gmail.com>
2013-04-18 03:48:24 +0400

    ...

---
+++ ListViewEx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TelitTerminal
+{
+	public class ListViewEx : ListView
+	{
+		public ListViewEx()
+		{
+			SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
+		}
+	}
+}
